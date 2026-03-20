6db20c85c0e50e61a95c3450d2f7ea83b55e373c
Sergei Kolobov <sergei@kolobov.com>
2026-03-20 21:20:25 +0100

    docs: add docstrings to conanfile.py and test_package/conanfile.py
    Refs #5

--- conanfile.py
+++ conanfile.py
@@ -7,6 +7,8 @@ from conan.tools.files import copy, load
 
 
 class NumopsConan(ConanFile):
+    """Conan 2 recipe for the numops C++ arithmetic library."""
+
     name = "numops"
     license = "MIT"
     url = "https://github.com/skolobov/curly-enigma"
@@ -25,6 +27,7 @@ class NumopsConan(ConanFile):
     )
 
     def set_version(self):
+        """Read version from CMakeLists.txt unless overridden via --version."""
         if self.version:
             return
         content = load(self, os.path.join(self.recipe_folder, "CMakeLists.txt"))
@@ -33,32 +36,39 @@ class NumopsConan(ConanFile):
             self.version = match.group(1)
 
     def config_options(self):
+        """Remove fPIC option on Windows."""
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
+        """Remove fPIC when building shared libraries."""
         if self.options.shared:
             self.options.rm_safe("fPIC")
 
     def layout(self):
+        """Use standard CMake layout."""
         cmake_layout(self)
 
     def generate(self):
+        """Generate CMake toolchain with testing disabled."""
         tc = CMakeToolchain(self)
         tc.variables["BUILD_TESTING"] = False
         tc.generate()
 
     def build(self):
+        """Configure and build the library with CMake."""
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def package(self):
+        """Install headers, library, and license into the package folder."""
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
+        """Define library name and CMake target information for consumers."""
         self.cpp_info.libs = ["numops"]
         self.cpp_info.set_property("cmake_file_name", "numops")
         self.cpp_info.set_property("cmake_target_name", "numops::numops")
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -6,21 +6,27 @@ from conan.tools.build import can_run
 
 
 class NumopsTestConan(ConanFile):
+    """Consumer project that validates the numops Conan package."""
+
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
+        """Declare the package under test as a dependency."""
         self.requires(self.tested_reference_str)
 
     def layout(self):
+        """Use standard CMake layout."""
         cmake_layout(self)
 
     def build(self):
+        """Configure and build the consumer project."""
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def test(self):
+        """Run the consumer executable to validate library functionality."""
         if can_run(self):
             cmd = os.path.join(self.cpp.build.bindir, "example")
             self.run(cmd, env="conanrun")
